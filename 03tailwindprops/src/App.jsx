7a977f97909172ed18a817e5485880b4e8055a0d
SAHIL <sahilnimje12345@gmail.com>
2026-01-04 21:34:35 +0530

    first commit

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import Card from "./components/Card";
+function App() {
+  let myObj = {
+    username: "sahil",
+    age: 21,
+  };
+  let newArr = [1, 2, 3, 4];
+  return (
+    <>
+      <h1
+        className="bg-green-400 text-black p-4
+          rounded-xl mb-4"
+      >
+        Tailwind test
+      </h1>
+      <Card username="chai aur code" btnText="click MEe" />
+      <Card username="BMW M5" btnText="visit me" />
+    </>
+  );
+}
+
+export default App;
